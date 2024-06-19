afea8e5a868d2144de34abdb4bca770f8585093f
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2024-06-19 17:39:33 +0530

    added password validation

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -117,71 +117,72 @@ const handleLogout = async (req, res) => {
     res.status(204).json({ 'message': 'No content' });
 };
 
- const forgetPassword = async (req, res) => {
+const forgetPassword = async (req, res) => {
     try {
-      const user = await User.findOne({ mail: req.body.mail });
-  
-      if (!user) {
-        return res.status(404).send({ message: "User not found" });
-      }
-  
-      const token = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "10m",});
-  
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: process.env.EMAIL,
-          pass: process.env.PASSWORD_APP_EMAIL,
-        },
-      });
-  
-      const mailOptions = {
-        from: process.env.EMAIL,
-        to: req.body.mail,
-        subject: "Reset Password",
-        html: `<h1>Reset Your Password</h1>
+        const user = await User.findOne({ mail: req.body.mail });
+
+        if (!user) {
+            return res.status(404).send({ message: "User not found" });
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m", });
+
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD_APP_EMAIL,
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL,
+            to: req.body.mail,
+            subject: "Reset Password",
+            html: `<h1>Reset Your Password</h1>
       <p>Click on the following link to reset your password:</p>
       <a href="http://localhost:3500/reset/${token}">http://localhost:3500/reset/${token}</a>
       <p>The link will expire in 10 minutes.</p>
       <p>If you didn't request a password reset, please ignore this email.</p>`,
-      };
-  
-      transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-          return res.status(500).send({ message: err.message });
-        }
-        res.status(200).send({ message: "Email sent" });
-      });
-    } catch (err) {
-      res.status(500).send({ message: err.message });
-    }
-  };
+        };
 
-   const resetPassword = async (req, res) => {
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                return res.status(500).send({ message: err.message });
+            }
+            res.status(200).send({ message: "Email sent" });
+        });
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+};
+
+const resetPassword = async (req, res) => {
     try {
-      const decodedToken = jwt.verify(
-        req.params.token,
-        process.env.ACCESS_TOKEN_SECRET
-      );
-  
-      if (!decodedToken) {
-        return res.status(401).send({ message: "Invalid token" });
-      }
-  
-      const user = await User.findOne({ _id: decodedToken.userId });
-      if (!user) {
-        return res.status(401).send({ message: "no user found" });
-      }
-      
-      const newPassword = await bcrypt.hash(req.body.newPassword, 10);
-  
-      user.password = newPassword;
-      await user.save();
-  
-      res.status(200).send({ message: "Password updated" });
-    } catch (err) {
-      res.status(500).send({ message: err.message });
-    }
-  };
+        const decodedToken = jwt.verify(
+            req.params.token,
+            process.env.ACCESS_TOKEN_SECRET
+        );
 
-module.exports = { handleLogin, handleSignup, handleLogout, forgetPassword, resetPassword};
+        if (!decodedToken) {
+            return res.status(401).send({ message: "Invalid token" });
+        }
+
+        const user = await User.findOne({ _id: decodedToken.userId });
+        if (!user) {
+            return res.status(401).send({ message: "no user found" });
+        }
+
+        if (req.body.newPassword.length < 8) return res.status(400).json({ 'message': 'password must have minimun length of 8 characters' });
+        const newPassword = await bcrypt.hash(req.body.newPassword, 10);
+
+        user.password = newPassword;
+        await user.save();
+
+        res.status(200).send({ message: "Password updated" });
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+};
+
+module.exports = { handleLogin, handleSignup, handleLogout, forgetPassword, resetPassword };
